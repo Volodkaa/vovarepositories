d8e72e8ddcb88e74aa5619255734c1dd8c69d697
Volodkaa <vladim.andrianov2005@yandex.ru>
2023-10-14 14:56:02 +0300

    Initial Commit

---
+++ vova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vova
+//
+//  Created by Вова Андрианов on 14.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
